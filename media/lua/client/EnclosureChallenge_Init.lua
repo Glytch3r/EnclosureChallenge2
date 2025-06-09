b583fd9791c1d7f14f67f86191c08c86bd5025d1
Glytch3r <glytch3r.arts@gmail.com>
2025-06-09 21:05:04 +0800

    Create EnclosureChallenge_Init.lua

---
+++ media/lua/client/EnclosureChallenge_Init.lua
@@ -0,0 +1,129 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							          |
+|                       		                                    														 	  |
+|                       	Discord:    Glytch3r#1337 / glytch3r															      |
+|                       		                                    														 	  |
+|                       	Support:    https://ko-fi.com/glytch3r														    	  |
+|_______________________________________________________________________________________________________________________________-]]
+
+EnclosureChallenge = EnclosureChallenge or {}
+
+
+EnclosureChallenge.EnclosureSize = 189
+EnclosureChallenge.MarkerCache = {}
+-----------------------    init*        ---------------------------
+
+function EnclosureChallenge.initChallengeData(pl)
+    pl = pl or getPlayer()
+    if not pl then return end
+    local ec = EnclosureChallenge.getData()
+    if ec.EnclosureX ~= nil or ec.enclosureX ~= nil or ec.EnclosureY ~= nil or ec.enclosureY ~= nil then
+        EnclosureChallenge.resetData()
+        return
+    end
+
+
+    ec.UnlockPoints = ec.UnlockPoints or SandboxVars.EnclosureChallenge.StartingUnlockPoints or 1
+    ec.RewardChoice = ec.RewardChoice or 0
+    ec.ChallengeTime = ec.ChallengeTime or 0
+    ec.RemoteChallenge = ec.RemoteChallenge or ""
+    ec.RemoteWins = ec.RemoteWins or 0
+    ec.Challenges = ec.Challenges or {}
+    ec.Conquered = ec.Conquered or {}
+    ec.PrevCoord = {}
+    ec.Rebound = ec.Rebound or { x = nil, y = nil, z = nil }
+end
+
+
+Events.OnPlayerDeath.Add(function()
+	local pl = getPlayer()
+	local user = pl:getUsername()
+	if not EnclosureChallenge.isChallenger(pl)  then return end
+	if EnclosureChallenge.isShouldAnnounce() then
+
+
+
+	    local enc = EnclosureChallenge.getEnclosureXY(  pl:getX(),   pl:getY())
+        if not enc then return end
+
+        local encStr = EnclosureChallenge.getEnclosureStr(pl)
+		local posStr = (EnclosureX and EnclosureY) and ": [" .. tostring(encStr) .. "]" or ""
+		if not SandboxVars.EnclosureChallenge.CoordNotif then
+			posStr = ""
+		end
+
+		local msg = tostring(user) .. " " .. getText("ContextMenu_EnclosureChallenge_Fail").. "  ".. tostring(posStr)
+		if isClient() then
+			processGeneralMessage(msg)
+		else
+			pl:setHaloNote(msg, 150, 250, 150, 900)
+		end
+	end
+	--EnclosureChallenge.setChallenge(false, false)
+end)
+
+
+
+Events.OnCreatePlayer.Add(function()
+    if isIngameState() then
+        local pl = getPlayer()
+        if not pl then return end
+
+        EnclosureChallenge.initChallengeData(pl)
+        EnclosureChallenge.setMarkers(pl)
+--[[         if EnclosureChallenge.isChallenger(pl) then
+        end
+ ]]
+
+		--LuaEventManager.AddEvent("OnClockUpdate")
+		--EnclosureChallenge.clock()
+
+
+		EnclosureChallenge.showDraw = true
+	end
+end)
+
+-----------------------       ---------------------------
+LuaEventManager.AddEvent("OnEnclosureChange")
+function EnclosureChallenge.updateMarkers()
+
+
+	if getCore():getDebug() then
+        print('OnEnclosureChange')
+	end
+
+    local pl = getPlayer()
+    if not pl then return end
+
+    local enc =  EnclosureChallenge.getEnclosure(pl)
+    EnclosureChallenge.setMarkers(pl, false)
+
+    EnclosureChallenge.addChallengeSymbols(pl)
+
+    local x = pl:getX()
+    local y = pl:getY()
+    local  midX, midY = EnclosureChallenge.getEnclosureMidXY(x, y, pl)
+    EnclosureChallenge.drawEnclosureGrid(midX, midY)
+    EnclosureChallenge.drawEnclosureGridOverlay(minimap, midX, midY)
+end
+Events.OnEnclosureChange.Add(EnclosureChallenge.updateMarkers)
+
+--[[_____________________________________________________________________________________________________________________________
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
